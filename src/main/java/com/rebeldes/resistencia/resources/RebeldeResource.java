c052df7c403263d93bf89a25ec12d03f1700000c
Lucas Queiroz <lucasmedeirosq@gmail.com>
2019-06-07 23:24:56 -0300

    Habilitando o crossorigin

--- src/main/java/com/rebeldes/resistencia/resources/RebeldeResource.java
+++ src/main/java/com/rebeldes/resistencia/resources/RebeldeResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,7 @@ import com.rebeldes.resistencia.services.InventarioService;
 import com.rebeldes.resistencia.services.RebeldeService;
 import com.rebeldes.resistencia.services.TraidoresService;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value="/api")
 public class RebeldeResource {
--- src/main/java/com/rebeldes/resistencia/resources/RelatorioResource.java
+++ src/main/java/com/rebeldes/resistencia/resources/RelatorioResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import com.rebeldes.resistencia.services.InventarioService;
 import com.rebeldes.resistencia.services.RebeldeService;
 import com.rebeldes.resistencia.services.TraidoresService;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value="/api/relatorios")
 public class RelatorioResource {   // destinado para resoulver os pedidos de relatorios
